6dbf6875492b97401bd4fa6b2b4285d623dbfb2a
madF <58264400+fakih06@users.noreply.github.com>
2022-07-28 09:37:08 +0800

    add materi screen
    nambah halaman materi

--- lib/utils/exports.dart
+++ lib/utils/exports.dart
@@ -1,8 +1,9 @@
 export 'package:flutter/material.dart';
 
 //Views
-
+export 'package:unm_lms/materi_screen.dart';
 export 'package:unm_lms/views/home_screen.dart';
 export 'package:unm_lms/views/login_signup/login_screen.dart';
 export 'package:unm_lms/views/login_signup/signup_sreen.dart';
 export 'package:unm_lms/views/welcome_Screen.dart';
+export 'package:unm_lms/views/matkul_screen.dart';
